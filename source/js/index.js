29eb08552e72e6de560570b8f11f6aeaa34b59c4
Rakshith Bellare <rakshith_bellare@outlook.com>
2021-12-23 13:43:58 +0530

    Removed console logs

--- source/js/index.js
+++ source/js/index.js
@@ -1,7 +1,7 @@
 import translations from "./languages.js";
-console.log(navigator.language || navigator.userLanguage)
+
 const language = navigator.language || navigator.userLanguage;
-console.log(translations)
+
 if(translations[language]) {
   app.innerText = translations[language]
 }
